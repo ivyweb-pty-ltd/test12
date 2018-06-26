45236b5b6bf4109e2676466bd1b3f2737f6ecd81
Daniel Maimba <daniel@strategicdimensions.co.za>
2018-06-26 23:10:27 +0200

    [IMP] allow portal user to upload helpdesk ticket document
    [IMP] allow portal user to upload helpdesk ticket document

--- crm_attooh/models/helpdesk_portal.py
+++ crm_attooh/models/helpdesk_portal.py
@@ -1,36 +1,35 @@
-from odoo import http, _
+from odoo import http
 from odoo.http import request
 import base64
 from odoo.addons.portal.controllers.portal import get_records_pager, pager as portal_pager, CustomerPortal
 
 class CustomerPortal(CustomerPortal):
     
-    @http.route('/upload_document_helpdesk', type='http', auth="public", website=True)
-    def upload_document_helpdesk(self, **post):
-        filename = post.get('doc_attachment').filename
-        file = post.get('doc_attachment')
-        attach_id = request.env['ir.attachment'].create({
-                        'name' : filename,
-                        'type': 'binary',
-                        'datas_fname':filename,
-                        'datas': base64.b64encode(file.read()),
-                        'document_available': True,
-                        'document_type': post.get('doc_type'),
-                        'res_id': post.get('ticket_id', False),
-                        'res_model': 'helpdesk.ticket',
-#                         'create_uid': request.env.user.id,
-                    })
-        attach_id._compute_res_name()
-        print ('\n\nattach_id', attach_id)
-        mail_msg_id = request.env['mail.message'].sudo().create({'attachment_ids': [(4, attach_id.id)],
-                                                       'author_id': request.env.user.partner_id.id,
-                                                       'model': 'helpdesk.ticket',
-                                                       'res_id': int(post.get('ticket_id', False)),
-                                                       'message_type': 'comment',
-                                                       'website_published': True,
-                                                       'create_uid': request.env.user.id,
-                                                       })
-        print ('\n\nmail_msg_id', mail_msg_id)
-#         mail_msg_id.sudo().update({'create_uid': request.env.user.id})
-#         print ('\n\ncurrent_user_id', request.env.user.id)
-        return request.redirect('/helpdesk/ticket/%s'%post.get('ticket_id'))
+    @http.route('/upload_document_helpdesk', type='http', auth="user", website=True)
+    def upload_document_helpdesk(self, ticket_id, doc_type, doc_attachment, **post):
+        user = request.env.user
+        domain = ['|', ('user_id', '=', user.id), ('partner_id', 'child_of', user.partner_id.commercial_partner_id.id), ('id', '=', int(ticket_id))]
+        ticket = request.env['helpdesk.ticket'].sudo().search(domain, limit=1)
+        if ticket:
+            filename = doc_attachment.filename
+            attach = request.env['ir.attachment'].sudo().create({
+                            'name': filename,
+                            'type': 'binary',
+                            'datas_fname': filename,
+                            'datas': base64.b64encode(doc_attachment.read()),
+                            'document_available': True,
+                            'document_type': doc_type,
+                            'res_id': int(ticket_id) or False,
+                            'res_model': 'helpdesk.ticket',
+                        })
+            attach._compute_res_name()
+            request.env['mail.message'].sudo().create({
+                'attachment_ids': [(4, attach.id)],
+                'author_id': request.env.user.partner_id.id,
+                'model': 'helpdesk.ticket',
+                'res_id': int(ticket_id) or False,
+                'message_type': 'comment',
+                'website_published': True,
+                'create_uid': request.env.user.id,
+            })
+            return request.redirect('/helpdesk/ticket/%s' % ticket_id)
